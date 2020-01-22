ff9fee4a5fee18d05c0e301dc02a4d89fd341ba2
vincentianxing <sky103034@gmail.com>
2020-01-22 17:57:47 +0800

    finish syllabus#create test and add shoulda gem

--- spec/controllers/cart_pages_controller_spec.rb
+++ spec/controllers/cart_pages_controller_spec.rb
@@ -1,10 +1,11 @@
 require "spec_helper"
 
 describe CartsController do
-  describe "GET 'view'" do
-    it "returns http success" do
-      get :show
-      response.should be_success
+
+  describe "GET #show" do
+    it "redirect to 404 if cart has not existed" do
+      get :show, id: 1
+      response.should redirect_to error_404_path
     end
     context "when the cart has courses" do
       cart = Cart.create
@@ -45,39 +46,11 @@ describe CartsController do
   end
 
   describe "PUT #update" do
-    course_arr = ["Computer Science", "CSCI", "214", "01", 
-                    "532", "Full Term", "", "202009",
-                    "", "Principles of Computer Science", 
-                    "Geitz, Bob"]
-    course = Course.build(course_arr)
-    course.save               
-    courses = { "f20_532" => "Principles of Computer Science" }
-
-    it "adds selected courses to the cart" do
-      cart = Cart.create
-      course.semcrn.should eq("f20_532")
-      put :update, id: cart.cartid, courses: courses, format: 'js'
-      assigns(:cart_courses).should eq(assigns(:cart).get_courses)
-      cart.delete
-    end
-=begin
-    context "returns current cart" do
-      it "when too many courses are selected" do
-        cart = Cart.create
-        cart_before = cart.get_courses
-        #16777215.times { |key| courses[key] = key }
-        put :update, id: cart.cartid, courses: courses, format: 'js'
-        assigns(:cart_courses).should eq(cart_before)
-        cart.delete
-      end
-=end
-    it "returns current cart when no course is selected" do
-      cart = Cart.create
-      cart_before = cart.get_courses
-      put :update, id: cart.cartid, courses: nil, format: 'js'
-      assigns(:cart_courses).should eq(cart_before)
-      cart.delete
-    end
+    
+  end
+  
+  describe "PUT #add" do
+    
   end
   
   describe "PUT #remove" do
@@ -97,4 +70,5 @@ describe CartsController do
   describe "GET #search" do
 
   end
+
 end
--- spec/controllers/syllabus_controller_spec.rb
+++ spec/controllers/syllabus_controller_spec.rb
@@ -2,17 +2,23 @@ require 'spec_helper'
 
 describe SyllabusController do
 
-  describe "GET syllabus#new" do
+  describe "GET #new" do
+    course_arr = ["Computer Science", "CSCI", "214", "01", 
+                    "532", "Full Term", "", "202009",
+                    "", "Principles of Computer Science", 
+                    "Geitz, Bob"]
     context "when course has no syllabus" do
       it "should make a new syllabus" do
-        course = Course.create
+        course = Course.build(course_arr)
+        course.save
         course.syllabus = nil
         expect{ get :new, id: course.id }.to change{ Syllabus.count }.by(1)
       end
     end
     context "when course has a syllabus" do
       it "should @syllabus to the syllabus" do
-        course = Course.create
+        course = Course.build(course_arr)
+        course.save
         course.syllabus = Syllabus.create
         get :new, id: course.id
         assigns(:syllabus).should eq(course.syllabus)
@@ -21,7 +27,7 @@ describe SyllabusController do
 
   end
 
-  describe "GET syllabus#show" do
+  describe "GET #show" do
     it "should redirect to the syllabus" do
       path = 'http://132.162.201.242/app/doc/README_FOR_APP.html'
       syllabus = Syllabus.new(path: path)
@@ -31,12 +37,13 @@ describe SyllabusController do
     end
   end
 
-  describe "POST syllabus#create" do
-    it "should flash" do
-      syllabus = Syllabus.new
+  describe "POST #create" do
+    it "should flash" do # cannot test no flash when path invalid, due to the structure
+      path =  'http://132.162.201.242/app/doc/README_FOR_APP.html'
+      syllabus = Syllabus.new(path: path)
       syllabus.save
-      post :create, id: syllabus.id
-      expect{flash[:success]}.should_not exist
+      post :create, id: syllabus.id, syllabus: { path: path }
+      should set_flash[:success]
     end
   end
 
