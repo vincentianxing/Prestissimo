b124ce3e3afaacf59e57ad65cdc85919e37458d2
vincentianxing <vzhu@oberlin.edu>
2020-01-24 03:44:53 -0500

    finish carts#update test

--- spec/controllers/cart_pages_controller_spec.rb
+++ spec/controllers/cart_pages_controller_spec.rb
@@ -46,11 +46,39 @@ describe CartsController do
   end
 
   describe "PUT #update" do
-    
-  end
-  
-  describe "PUT #add" do
-    
+    course_arr = ["Computer Science", "CSCI", "214", "01", 
+                    "532", "Full Term", "", "202009",
+                    "", "Principles of Computer Science", 
+                    "Geitz, Bob"]
+    course = Course.build(course_arr)
+    course.save               
+    courses = { "f20_532" => "Principles of Computer Science" }
+
+    it "adds selected courses to the cart" do
+      cart = Cart.create
+      course.semcrn.should eq("f20_532")
+      put :update, id: cart.cartid, courses: courses, format: 'js'
+      assigns(:cart_courses).should eq(assigns(:cart).get_courses)
+      cart.delete
+    end
+=begin
+    context "returns current cart" do
+      it "when too many courses are selected" do
+        cart = Cart.create
+        cart_before = cart.get_courses
+        #16777215.times { |key| courses[key] = key }
+        put :update, id: cart.cartid, courses: courses, format: 'js'
+        assigns(:cart_courses).should eq(cart_before)
+        cart.delete
+      end
+=end
+    it "returns current cart when no course is selected" do
+      cart = Cart.create
+      cart_before = cart.get_courses
+      put :update, id: cart.cartid, courses: nil, format: 'js'
+      assigns(:cart_courses).should eq(cart_before)
+      cart.delete
+    end
   end
   
   describe "PUT #remove" do
