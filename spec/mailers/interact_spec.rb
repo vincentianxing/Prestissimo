0b8492a49ae2cb5601ba3551a685a24fa153839f
Ivy <larlesienne@outlook.com>
2020-01-28 17:22:58 -0500

    some interact tests

--- spec/mailers/interact_spec.rb
+++ spec/mailers/interact_spec.rb
@@ -1,5 +1,22 @@
 require "spec_helper"
 
 describe Interact do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'reveal_handle_user' do
+    let(:user){ mock_model User, fname: 'Ivy', email: 'ifu@oberlin.edu'}
+    let(:admin){ mock_model User, fname: 'Leo', email: 'larlesienne@outlook.com', admin: True}
+  end
+  describe 'send_e' do
+    let(:user){ mock_model User, fname: 'Ivy', lname: 'Fu', email: 'ifu@oberlin.edu'}
+    let(:mail){Interact.send_e("Ho","Hello world", user)}
+
+    it 'sends an email' do
+      expect { ActionMailer::Base.deliveries.count }
+        .to change.by(1)
+    end
+
+    it 'renders the subject' do
+      expect(mail.subject.to eql("Ho: Hello world Fu")
+    end
+  end
 end
+
