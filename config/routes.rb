bdf0bd537a2b8b650fed8fe4249d8b31abffec33
Max Mitchell <mmitchel@oberlin.edu>
2020-02-11 17:32:00 -0500

    Reset changes to routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -28,9 +28,9 @@ Prestissimo::Application.routes.draw do
   scope "(/:commentable_type/:commentable_id)" do
     resources :comments do
       member do
-        get 'reply_form'
-        post 'reply'
-        patch 'hide_remove'
+	get 'reply_form'
+	post 'reply'
+	put 'hide_remove'
       end
     end
   end
@@ -44,9 +44,9 @@ Prestissimo::Application.routes.draw do
   resources :settings, only: [] do
     collection do
       get 'change'
-      patch 'update_all'
+      put 'update_all'
       get 'edit_motd'
-      patch 'set_motd'
+      put 'set_motd'
     end
   end
 
@@ -54,9 +54,9 @@ Prestissimo::Application.routes.draw do
   resources :carts, only: [:create, :update, :destroy, :show] do
     member do
       scope "(/:semcrn)" do
-        patch 'add'
-        patch 'remove'
-        patch 'clear'
+        put 'add'
+        put 'remove'
+        put 'clear'
       end
       get 'mail_to'
       get 'search'
@@ -86,95 +86,95 @@ Prestissimo::Application.routes.draw do
   root to: 'courses#index'
 
   # route for search
-  match '/courses/search', to: 'courses#search', as: :course_search, via: [:get, :post]
+  match '/courses/search', to: 'courses#search', as: :course_search
 
   # routes for editing courses as a professor
-  match '/courses/(:semcrn)/edit', to: 'courses#edit', as: :course_edit, via: [:get, :post]
-  match '/courses/:id/update', to: 'courses#update', as: :update_course, via: [:get, :post]
+  match '/courses/(:semcrn)/edit', to: 'courses#edit', as: :course_edit
+  match '/courses/:id/update', to: 'courses#update', as: :update_course
 
   # routes for user search
-  match '/users/search', to: 'users#search', as: :user_search, via: [:get, :post]
-  match '/users', to: 'users#index', as: :users, via: [:get, :post]
+  match '/users/search', to: 'users#search', as: :user_search
+  match '/users', to: 'users#index', as: :users
 
 
   # routes for static pages
-  get '/about', to: 'static_pages#about'
-  get '/reqs', to: 'static_pages#reqs'
-  get '/help', to: 'static_pages#help'
-  get '/guidelines', to: 'static_pages#guidelines'
-  get '/help/basics', to: 'static_pages#basics'
-  get '/help/features', to: 'static_pages#features'
-  get '/help/questions', to: 'static_pages#questions'
-  get '/help/advanced', to: 'static_pages#basics' #There used to be a Basics and an Advanced search page, but these were combined
-  get '/secret', to: 'static_pages#secret'
-  get '/qmailer', to: 'static_pages#questions_mailer'
-  get '/emailer', to: 'static_pages#error_mailer'
-  get '/help/accounts', to: 'static_pages#accounts'
-  get '/help/cart', to: 'static_pages#cart'
-  get '/help/comments', to: 'static_pages#comments'
-  get '/help/hubpages', to: 'static_pages#hubpages'
-  get '/help/professors', to: 'static_pages#professors'
-  get '/help/phone', to: 'static_pages#phone'
-  get '/help/search', to: 'static_pages#search'
-  get '/help/editcourse', to:'static_pages#editcourse'
-  get '/help/error', to:'static_pages#nameerror', as: :nameerror
-  get '/links', to: 'static_pages#links'
+  match '/about', to: 'static_pages#about'
+  match '/reqs', to: 'static_pages#reqs'
+  match '/help', to: 'static_pages#help'
+  match '/guidelines', to: 'static_pages#guidelines'
+  match '/help/basics', to: 'static_pages#basics'
+  match '/help/features', to: 'static_pages#features'
+  match '/help/questions', to: 'static_pages#questions'
+  match '/help/advanced', to: 'static_pages#basics' #There used to be a Basics and an Advanced search page, but these were combined
+  match '/secret', to: 'static_pages#secret'
+  match '/qmailer', to: 'static_pages#questions_mailer'
+  match '/emailer', to: 'static_pages#error_mailer'
+  match '/help/accounts', to: 'static_pages#accounts'
+  match '/help/cart', to: 'static_pages#cart'
+  match '/help/comments', to: 'static_pages#comments'
+  match '/help/hubpages', to: 'static_pages#hubpages'
+  match '/help/professors', to: 'static_pages#professors'
+  match '/help/phone', to: 'static_pages#phone'
+  match '/help/search', to: 'static_pages#search'
+  match '/help/editcourse', to:'static_pages#editcourse'
+  match '/help/error', to:'static_pages#nameerror', as: :nameerror
+  match '/links', to: 'static_pages#links'
 
   # other user and sessions routes
-  match '/signin', to: 'users#new', as: :signin, via: [:get, :post]
+  match '/signin', to: 'users#new', as: :signin
   match '/signout', to: 'sessions#destroy', via: :delete
-  match '/create', to: 'users#create', as: :create_user, via: [:get, :post]
-  match '/users/'+':email'.split('@')[0], to: 'users#show', as: :user, via: [:get, :post]
-  match '/users/'+':email'.split('@')[0]+'/settings', to: 'users#edit', as: :settings, via: [:get, :post]
-  match '/users/'+':email'.split('@')[0] + '/schedule', to: 'users#schedule', as: :schedule, via: [:get, :post]
-  match '/update/:id', to: 'users#update', as: :update, via: [:get, :post]
-  match '/delete/:id', to: 'users#destroy', as: :delete_user, via: [:get, :post]
-  match '/users/confirm/delete/'+':email'.split('@')[0], to: 'users#confirm_destroy', as: :confirm_destroy, via: [:get, :post]
-  match '/users/self_delete/:id', to: 'users#self_destroy', as: :self_destroy, via: [:get, :post]
+  match '/create', to: 'users#create', as: :create_user
+  match '/users/'+':email'.split('@')[0], to: 'users#show', as: :user
+  match '/users/'+':email'.split('@')[0]+'/settings', to: 'users#edit', as: :settings
+  match '/users/'+':email'.split('@')[0] + '/schedule', to: 'users#schedule', as: :schedule
+  match '/update/:id', to: 'users#update', as: :update
+  match '/delete/:id', to: 'users#destroy', as: :delete_user
+  match '/users/confirm/delete/'+':email'.split('@')[0], to: 'users#confirm_destroy', as: :confirm_destroy
+  match '/users/self_delete/:id', to: 'users#self_destroy', as: :self_destroy
 
   # handle routes
-  match '/commenters/update/:id', to: 'handles#update', as: :update_handle, via: [:get, :post]
-  match '/commenters', to: 'handles#index', as: :handles, via: [:get, :post]
-  match '/commenters/search', to: 'handles#search', as: :handle_search, via: [:get, :post]
-  match '/commenters/:username', to: 'handles#show', as: :show_handle, via: [:get, :post]
-  match '/commenters/mute/:id', to: 'handles#mute', as: :mute_handle, via: [:get, :post]
-  match '/commenters/reveal/:id', to: 'handles#reveal', as: :reveal_user, via: [:get, :post]
+  match '/commenters/update/:id', to: 'handles#update', as: :update_handle
+  match '/commenters', to: 'handles#index', as: :handles
+  match '/commenters/search', to: 'handles#search', as: :handle_search
+  match '/commenters/:username', to: 'handles#show', as: :show_handle
+  match '/commenters/mute/:id', to: 'handles#mute', as: :mute_handle
+  match '/commenters/reveal/:id', to: 'handles#reveal', as: :reveal_user
 
   #routes related to showing/hiding the search help box
-  match '/hide_search', to: 'sessions#hide_search', via: [:get, :post]
-  match '/click_hide_search', to: 'sessions#click_hide_search', via: [:get, :post]
+  match '/hide_search', to: 'sessions#hide_search'
+  match '/click_hide_search', to: 'sessions#click_hide_search'
 
   #routes related to toggling html/mobile layout
-  match '/mobile_format', to: 'sessions#mobile_format', via: [:get, :post]
-  match '/click_mobile_format', to: 'sessions#click_mobile_format', via: [:get, :post]
+  match '/mobile_format', to: 'sessions#mobile_format'
+  match '/click_mobile_format', to: 'sessions#click_mobile_format'
 
   # administrative route
-  match '/admin', to: 'admins#admin', as: :admin_page, via: [:get, :post]
-  match '/admin/expire_sessions', to: 'admins#expire_sessions', as: :expire_sessions, via: [:get, :post]
+  match '/admin', to: 'admins#admin', as: :admin_page
+  match '/admin/expire_sessions', to: 'admins#expire_sessions', as: :expire_sessions
 
   # route for professor page creation -- NOT USED, AND OUT OF DATE
   # match '/professors/populate', to: 'professors#populate', as: :populate_professors
 
   # more professor routes
-  match '/professors/search', to: 'professors#search', as: :professor_search, via: [:get, :post]
-  match '/professors/(:fname)_(:lname)/edit', to: 'professors#edit', as: :edit_professor, :fname => /.*/, via: [:get, :post]
-  match '/professors/(:fname)_(:lname)/toggleactive', to: 'professors#toggle_active', as: :toggle_active_professor, :fname => /.*/, via: [:get, :post]
-  match '/professors/(:fname)_(:lname)', to: 'professors#show', as: :show_professor, :fname => /.*/, via: [:get, :post]
-  match '/professors/:id/update', to: 'professors#update', as: :update_professor, via: [:get, :post]
-  match '/professors/(:fname)_(:lname)/contentpreview', to: 'professors#preview', as: :professor_content_preview, via: [:get, :post]
-  match '/professors/(:fname)_(:lname)/courses', to: 'professors#professor_courses', as: :professor_courses, via: [:get, :post]
+  match '/professors/search', to: 'professors#search', as: :professor_search
+  match '/professors/(:fname)_(:lname)/edit', to: 'professors#edit', as: :edit_professor, :fname => /.*/
+  match '/professors/(:fname)_(:lname)/toggleactive', to: 'professors#toggle_active', as: :toggle_active_professor, :fname => /.*/
+  match '/professors/(:fname)_(:lname)', to: 'professors#show', as: :show_professor, :fname => /.*/
+  match '/professors/:id/update', to: 'professors#update', as: :update_professor
+  match '/professors/(:fname)_(:lname)/contentpreview', to: 'professors#preview', as: :professor_content_preview
+  match '/professors/(:fname)_(:lname)/courses', to: 'professors#professor_courses', as: :professor_courses
 
   # syllabus routes
-  match '/syllabus/new/:id', to: 'syllabus#new', as: :new_syllabus, via: [:get, :post]
-  match '/syllabus/create/:id', to: 'syllabus#create', as: :create_syllabus, via: [:get, :post]
-  match '/syllabus/:id', to: 'syllabus#show', as: :syllabus, via: [:get, :post]
+  match '/syllabus/new/:id', to: 'syllabus#new', as: :new_syllabus
+  match '/syllabus/create/:id', to: 'syllabus#create', as: :create_syllabus
+  match '/syllabus/:id', to: 'syllabus#show', as: :syllabus
 
   # Routes for error pages
-  get '/404', to: 'errors#not_found', as: :error_404
-  get '/422', to: 'errors#server_error'
-  get '/500', to: 'errors#server_error'
+  match '/404', to: 'errors#not_found', as: :error_404
+  match '/422', to: 'errors#server_error'
+  match '/500', to: 'errors#server_error'
   # Random route for clearing cookies, 
-  match '/clear_cookies', to: 'errors#clear', as: :clear_cookies, via: [:get, :post]
+  match '/clear_cookies', to: 'errors#clear', as: :clear_cookies
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
