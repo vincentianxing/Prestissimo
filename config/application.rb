b35077879420b8ca9fae28ba42b4dab1bf60bdfc
Max Mitchell <mmitchel@oberlin.edu>
2020-03-12 11:05:19 -0400

    Update to 2.2, uncomment require md5

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
-# require 'digest/md5'
+require 'digest/md5'
 
 
 # Require the gems listed in Gemfile, including any gems
