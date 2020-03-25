d3753ff89e66f8f3db20481a26bf3de3c807993a
Max Mitchell <mmitchel@oberlin.edu>
2020-03-24 23:21:52 -0400

    Remove mysql adapter

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,6 @@
 # Load the rails application
 require_relative 'application'
 
-# Patches a mysql error where primary keys default to NULL
-require File.expand_path('../initializers/abstract_mysql2_adapter.rb', __FILE__)
-
 # Initialize the rails application
 Rails.application.initialize!
 
