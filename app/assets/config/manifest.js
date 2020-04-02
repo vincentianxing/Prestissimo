4daab80f1b31d833d46441cf8b72495195f210fc
Max Mitchell <mmitchel@oberlin.edu>
2020-04-02 13:26:58 -0400

    Fix mobile

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,6 @@
 //= link_tree ../images
 //= link application.css
 //= link application.js
-//= link application_mobile.js
+//= link mobile/application_mobile.js
 //= link errors.css
 //= link mobile.css
