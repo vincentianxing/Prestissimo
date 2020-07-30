7e76c28e95d32e6240d977ff978dbfa9a2e583a1
Ava Simon <bsimon@oprestissimo.info>
2020-07-30 11:58:03 -0400

    Update schema comments and rdoc

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -7,18 +7,21 @@ end
 #
 # Table name: professors
 #
-#  id         :integer(4)      not null, primary key
-#  created_at :datetime        not null
-#  updated_at :datetime        not null
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
 #  email      :string(255)
 #  fname      :string(255)
 #  lname      :string(255)
 #  nickname   :string(255)
-#  status     :string(255)     default("active")
+#  status     :string(255)      default("active")
 #  office     :string(255)
 #  phone      :string(255)
 #  contact    :string(255)
 #  url        :string(255)
-#  content    :text
+#  content    :text(65535)
+#  userid     :string(255)
+#  can_edit   :boolean          default("1")
+#  my_classes :string(255)
 #
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,24 +7,24 @@ end
 #
 # Table name: users
 #
-#  id              :integer(4)      not null, primary key
-#  fname           :string(255)
-#  email           :string(255)
-#  password_digest :string(255)
-#  created_at      :datetime        not null
-#  updated_at      :datetime        not null
-#  remember_token  :string(255)
-#  status          :string(255)     default("pending")
-#  nonce           :string(255)
-#  lname           :string(255)
-#  role            :string(255)
-#  nickname        :string(255)
-#  admin           :boolean(1)      default(FALSE)
-#  gender          :string(255)
-#  year            :string(255)
-#  nonce_expiry    :datetime
-#  privacy_prefs   :text
-#  notes           :text
-#  cart_id         :integer(4)
+#  id             :integer          not null, primary key
+#  fname          :string(255)
+#  email          :string(255)
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  remember_token :string(255)
+#  status         :string(255)      default("pending")
+#  lname          :string(255)
+#  role           :string(255)
+#  nickname       :string(255)
+#  admin          :boolean          default("0")
+#  year           :string(255)
+#  notes          :text(65535)
+#  privacy_prefs  :text(65535)
+#  cart_id        :integer
+#  second_major   :string(255)
+#  double_major   :string(255)
+#  major          :string(255)
+#  prof_id        :integer
 #
 
